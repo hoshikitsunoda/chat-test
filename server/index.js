ab08d65f78c1659ff5252ba3186aa9463e323a0e
seiki tsunoda <liquidohr@gmail.com>
2018-02-24 17:08:43 -0800

    Add io.emit in index.js #1

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg)
+    io.emit('chat message', msg)
   })
 })
 
